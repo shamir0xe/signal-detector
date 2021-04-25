bd007dc226a18ef6fd707a63ad9c9cb04d7037b5
shamir0xe <shamir0xe@gmail.com>
2021-04-26 01:25:19 +0430

    fixed config_reader bug

--- src/helpers/config/config_reader.py
+++ src/helpers/config/config_reader.py
@@ -7,8 +7,7 @@ from libs.PythonLibrary.files import File
 class ConfigReader:
     def __init__(self, sub_path: str = '') -> None:
         self.sub_path = sub_path
-        # debug_text(os.path.join('..', '..', '..', 'config', 'config.json'))
-        self.json = File.read_json(os.path.join('.', '.', '.', 'config', 'config.json'))
+        self.json = File.read_json(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', 'config', 'config.json')))
     
     def get(self, selector: str, default: Any = None) -> Any:
         value = JsonHelper.selector_get_value(self.json, self.sub_path + '.' + selector)
